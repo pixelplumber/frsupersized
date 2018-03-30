99a09a147b25d5530fde1cc451c6a3943c6a70b0
ffrewer <info@frankfrewer.de>
2018-03-30 19:42:59 +0200

    Clean ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "frsupersized".
-#
-# Auto generated 22-08-2015 10:40
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Supersized - full screen background slideshow',
 	'description' => 'This extension implements the jquery supersized plugin - full screen background slideshow  (http://buildinternet.com/project/supersized/ - MIT License / GPL License). Fully configurable via TS and Flexform. Can be used for a full sreen background / background slideshow and a background banner / banner slideshow. Realized with easy adaptable fluid templates.',
@@ -41,5 +31,3 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 );
-
-?>
